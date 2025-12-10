e403a0548e334fae83c1be06d019537394d290d6
13150 <jeremy.garcia@vtsd.com>
2025-12-10 07:59:12 -0500

    Java script calculator

--- Calc/calc.js
+++ Calc/calc.js
@@ -1,1 +1,12 @@
-1
+// List of buttons
+const buttons= document.querySelectorAll('button')
+
+// Input for displaying calculations
+const display = document.querySelector('.display')
+
+// Fpr each button, add an onclick method with teh calculate function
+buttons.forEach(
+    function(button){
+        button.addEventListener("click", calculate);
+    }
+);
